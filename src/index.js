b77b4b810909e4e5b5b22d7eb27f13a120422485
JeongYoung Lee <mojomoth@JeongYoungui-MacBook-Pro.local>
2019-09-18 20:05:23 +0900

    allow async/await

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import '@babel/polyfill';
 import dotenv from 'dotenv';
 import api from 'api';
 
@@ -5,8 +6,12 @@ import api from 'api';
 dotenv.config();
 
 const port = process.env.SERVER_PORT || 4500;
+const timeout = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-(() => {
+(async () => {
   console.log('Hello Starter!');
   api(port);
+
+  await timeout(1000);
+  console.log('Async/Await!');
 })();
